ccdb4f39cb22235e7fe73c3c0b3c47c918622a79
matambasavaraju <raj.b@futurviewsystems>
2026-05-31 19:16:40 +0530

    phone number changed

--- src/components/site/SiteLayout.tsx
+++ src/components/site/SiteLayout.tsx
@@ -8,7 +8,7 @@ import { fetchSetting, type ContactSettings } from "@/lib/cms";
 
 const FALLBACK: ContactSettings = {
   phone: "+91 7416889997",
-  whatsapp: "917416889997",
+  whatsapp: "7416889997",
   email: "matambasavaraju90@gmail.com",
   address: "Near Krishnaveni ITI, Anasuyamma Colony, Undavelly Village, Alampur Taluka, Telangana 509153",
   maps_url: "https://www.google.com/maps/place/Parameshwara+Engineering+Works+Undavelly,+14-34,+Undavelly,+D.Burdipad,+Telangana+509153/data=!4m2!3m1!1s0x3bb5e587404cfebf:0xadaae89eb2961ea4",
